1abed824010c83739486cca4ff53beff27ffdfc4
Kevin Bradwick <kevinbradwick@gmail.com>
2017-01-15 19:16:45 +0000

    fixed incorrect import package name to ProjectModule

--- src/main/resources/archetype-resources/src/main/java/guice/ProjectModule.java
+++ src/main/resources/archetype-resources/src/main/java/guice/ProjectModule.java
@@ -3,9 +3,10 @@
 #set( $symbol_escape = '\' )
 package ${package}.guice;
 
+import ${package}.guice.provider.WebDriverProvider;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
-import com.kodefoundry.guice.provider.WebDriverProvider;
 import org.openqa.selenium.WebDriver;
 
 public final class ProjectModule extends AbstractModule {
